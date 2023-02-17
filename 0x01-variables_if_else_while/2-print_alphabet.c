e0f01d2d912f0ee7d14c0761bc96ed4206fa33e2
Zeroshski <njeez07ten@gmail>
2023-02-17 15:42:47 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int 1;
+
+	for (1 = 0; 1 < 26; 1++)
+	{
+		putchar(alp[1]);
+	}
+	putchar("\n");
+	return (0);
+}
