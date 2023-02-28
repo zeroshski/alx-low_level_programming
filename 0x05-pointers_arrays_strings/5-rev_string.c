b75adb88c5eb8932efd3767936fdc49160143052
Zeroshski <njeez07ten@gmail>
2023-02-27 16:19:33 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,6 +18,6 @@ void print_rev(char *s)
 
 	/*print char from the last index as you decrement*/
 	for (--index; index >= 0; --index)
-		_putchar(s[index]);
-	_putchar('\n');
+		putchar(s[index]);
+	putchar('\n');
 }
