92b410c11cb20a14cba969b8daa548578ca289da
Zeroshski <njeez07ten@gmail>
2023-03-01 15:36:14 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
+n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
